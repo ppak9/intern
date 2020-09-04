4ef9bc0c49c7c37141a399fa2fa8cb9c5f4608a5
PeterPPark <petepp@naver.com>
2020-09-05 03:35:30 +0900

    finish 5week

--- 5주차/0.py
+++ 5주차/0.py
@@ -1,45 +1,40 @@
-# 4,5 xlsx로 묶은 이후 마지막 실습으로 변경 
-# 마지막 실습으로 변경
+# final 결과본
+
 import requests
-from bs4 import BeautifulSoup
-import openpyxl
+from bs4 import BeautifulSoup as bs
+from urllib.request import urlretrieve
+from urllib.request import urlopen
+from urllib.parse import quote_plus
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
 
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36'}
 
-test =[]
+url = 'https://search.shopping.naver.com/best100v2/main.nhn'
 
-# User-agent
-url = 'https://datalab.naver.com/keyword/realtimeList.naver'
 raw = requests.get(url,headers=headers)
-soup = BeautifulSoup(raw.content,'html.parser')
-result = soup.select('span.item_title_wrap')
-wb = openpyxl.Workbook()
 
-for item in result:
-    keyword = item.select_one('span.item_title').text.strip()
-    test.append(keyword)
-# 동시에 네이버 실시간 검색어 1위 기사 가지고 오기
-# 강의 내용: 예외처리 + load에 대한 설명
-try:
-    wb=openpyxl.load_workbook("navernews_xlsx")
-    sheet = wb.active
-    print("불러오기 완료")
-except:
-    wb = openpyxl.Workbook()
-    sheet = wb.active
-    sheet.append(["제목","언론사"])
+soup = bs(raw.text,'html.parser')
 
-# 강의 내용: 추가적인 html 구조 파악
-for n in range(1,100,10):
-    raw = requests.get('https://search.naver.com/search.naver?where=news&query='+test[0])
-    # url을 통해 기사 수집
-    soup = BeautifulSoup(raw.text,'html.parser')
-    articles = soup.select("ul.type01 > li")
+target = soup.select('p.cont > a')
 
-    for ar in articles:
-        title = ar.select_one("a._sp_each_title").text
-        company = ar.select_one('span._sp_each_source').text
-        print(title,company)
-        sheet.append([title,company])
+##### middle 1 #####
+for i in target:
+    url2 = i.attrs['href']
+    html = urlopen(url2)
+    soup2 = bs(html,'html.parser')
+    img = soup2.find_all('img')
 
-wb.save('navertv.xlsx')
+    n=1
+    for i in img:
+        imgUrl = i.attrs['src']
+        if 'https://ssl.pstatic.net' in imgUrl:
+            continue
+        fullname = 'poster/'+ str(n) +'.jpg'
+        urlretrieve(imgUrl,fullname)
+
+        n+=1
+print('완료!')
+
+
--- 5주차/9_edit.py
+++ 5주차/9_edit.py
@@ -1,47 +1,35 @@
 from urllib.request import urlretrieve
-import requests
-from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from urllib.parse import quote_plus
+# urlopen 과 requests 의 차이 언급
+from bs4 import BeautifulSoup as bs
 
-
-#############################################
-# 추가
+# 보안 ssl 얘기(간단히)
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
-#############################################
 
-# urlretrieve("https://imgnews.pstatic.net/image/015/2020/09/03/0004410118_001_20200903180406260.jpg?type=w647",
-#             "홍정욱.png")
-
-# 확장
 
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36'}
 base_url = 'https://search.naver.com/search.naver?where=image&sm=tab_jum&query='
-test = input('name?')
-url = base_url +quote_plus(test)
-# raw = requests.get(url,headers=headers)
+plus_url = input('name?')
+url = base_url +quote_plus(plus_url)
+
+# soup 다른 라이브러리 설명
 html = urlopen(url)
-# print(type(html))
-# raw = requests.get(url,headers=headers)
-# soup = BeautifulSoup(html,'html.parser')
-soup = BeautifulSoup(html,'html.parser')
-
-
-    # each = requests.get(url2, headers=headers)
-    # soup_each = BeautifulSoup(each.text,'html.parser')
+# html 그대로를 가지고 오는 방식
+soup = bs(html,'html.parser')
 img = soup.find_all(class_='_img')
-# print(img)
+# 다른 라이브러리 제시
 
+######여기서 끊기#####
+
+# 반복문 예시
+# logic을 통해 변경
 n=1
 for i in img:
     imgUrl = i['data-source']
-    # poster_src = i.attrs['data-source']
-    # 마지막 urlretrieve 저장
-    # print(imgUrl)
-    fullname = 'poster/'+ str(n) +'.jpg'
+    fullname = 'poster/'+ plus_url + str(n) +'.jpg'
     urlretrieve(imgUrl,fullname)
-#   print(poster_src)
     n+=1
 print('완료!')
   
